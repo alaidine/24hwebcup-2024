2e7dafce1ea6b4b2ff6fb092406c5359cccef235
alaidine <aa.alimdlinde@gmail.com>
2024-05-04 17:41:18 +0400

    add carousel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,50 @@
 import "@/styles/globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import type { AppProps } from "next/app";
 
+import { Button } from "@/components/ui/button";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <NavigationMenu className="bg-gray-800 text-black p-4">
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuLink href="/">
+              <Button variant="outline">Home</Button>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Button variant="secondary">
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </Button>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 }
